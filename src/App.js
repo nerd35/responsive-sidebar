fb69d500d609fa5e09474981981e959f41c83371
Umukoro Chinedu <umukorochinedu@gmail.com>
2021-07-19 14:26:57 -0700

    dashbard with react, htnl and css

--- src/App.js
+++ src/App.js
@@ -1,24 +1,54 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { SideBar } from "./components";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import { useState } from "react";
+
+const Dashboard = () => {
+  return <h1>Dashboard</h1>;
+};
+const Content = () => {
+  return <h1>Content</h1>;
+};
+const Design = () => {
+  return <h1>Design</h1>;
+};
+const Courses = () => {
+  return <h1>Courses</h1>;
+};
+const Video = () => {
+  return <h1>Video</h1>;
+};
 
 function App() {
+  const [inactive, setInactive] = useState(false);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Router>
+        <SideBar onCollapse={(inactive) => {
+          setInactive(inactive)
+        }}/>
+        <div className={`container ${inactive ? "inactive" : ""}`}>
+        <Switch>
+          <Route exact path={"/"}>
+            <Dashboard />
+          </Route>
+          <Route exact path={"/content"}>
+            <Content />
+          </Route>
+          <Route path={"/design"}>
+            <Design />
+          </Route>
+          <Route path={"/content/videos"}>
+            <Video />
+          </Route>
+          <Route path={"/content/courses"}>
+            <Courses />
+          </Route>
+        </Switch>
+        </div>
+      </Router>
+    </>
   );
 }
 
